b12645e59e9d819e5409b302f287a9e815f138ef
darisasubhash <subbusubbu8193@gmail.com>
2026-02-03 11:16:04 +0530

    [subhash] : Validating First Name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-       System.out.println("Hello and welcome to User Registration ");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter First Name ");
+        String firstName=scan.next();
+        String regex="^[A-Z][a-z]{2,}$";
+        if(firstName.matches(regex)){
+            System.out.println("Valid First Name ");
+        }
+        else{
+            System.out.println("Invalid First Name ");
+        }
     }
 }
