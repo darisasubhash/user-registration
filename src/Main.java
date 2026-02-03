2da3c525aaa84269a55b85dcb2be805ce4f028ba
darisasubhash <subbusubbu8193@gmail.com>
2026-02-03 11:22:48 +0530

    [subhash] : UC2 - validating lastname

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan=new Scanner(System.in);
-        System.out.println("Enter First Name ");
+        System.out.println("Enter Last Name ");
         String firstName=scan.next();
         String regex="^[A-Z][a-z]{2,}$";
         if(firstName.matches(regex)){
-            System.out.println("Valid First Name ");
+            System.out.println("Valid Last Name ");
         }
         else{
-            System.out.println("Invalid First Name ");
+            System.out.println("Invalid Last Name ");
         }
     }
 }
