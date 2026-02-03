2be74fd8c243154df96394d02be6397513312cc6
darisasubhash <subbusubbu8193@gmail.com>
2026-02-03 11:31:29 +0530

    [subhash] : UC2-updating the logic

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan=new Scanner(System.in);
-        System.out.println("Enter Last Name ");
+        System.out.println("Enter First Name ");
         String firstName=scan.next();
-        String regex="^[A-Z][a-z]{2,}$";
-        if(firstName.matches(regex)){
+        String regex1="^[A-Z][a-z]{2,}$";
+        if(firstName.matches(regex1)){
+            System.out.println("Valid First Name ");
+        }
+        else{
+            System.out.println("Invalid First Name ");
+        }
+        System.out.println("Enter Last Name ");
+        String lastName=scan.next();
+        String regex2="^[A-Z][a-z]{2,}$";
+        if(firstName.matches(regex2)){
             System.out.println("Valid Last Name ");
         }
         else{
