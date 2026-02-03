0ee936373d6ff55a18ba7157984650938226fb58
darisasubhash <subbusubbu8193@gmail.com>
2026-02-03 17:20:41 +0530

    [Subhash] : UC-9 checking email samples provided seperately

--- src/Main.java
+++ src/Main.java
@@ -25,14 +25,15 @@ public class Main {
         System.out.println("Enter Email ");
         String email= scan.next();
         scan.nextLine();
-        String regex3="^[a-z]{3,}(\\.[a-z]{3,})?@[a-z]{2,}\\.[a-z]{2,}(\\.[a-z]{2,})?$";
+        String regex3="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[A-Za-z]{2,}){1,2}$";
         if(email.matches(regex3)){
             System.out.println("Valid Email ");
         }
         else{
             System.out.println("Invalid Email ");
         }
-        System.out.println("Enter MObile Number with Country Code ");
+        runTestCases(regex3);
+        System.out.println("Enter Mobile Number with Country Code ");
         String mobile= scan.nextLine();
         String regex4="[0-9]{2}\\s[0-9]{10}";
         if(mobile.matches(regex4)){
@@ -49,6 +50,42 @@ public class Main {
         }
         else{
             System.out.println("Invalid Password ");
+        }
+    }
+    private static void runTestCases(String regex){
+        String[] validEmails = {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+        };
+        String[] invalidEmails = {
+                "abc",
+                "abc@.com.my",
+                "abc123@gmail.a",
+                "abc123@.com",
+                "abc123@.com.com",
+                ".abc@abc.com",
+                "abc()*@gmail.com",
+                "abc@%*.com",
+                "abc..2002@gmail.com",
+                "abc.@gmail.com",
+                "abc@abc@gmail.com",
+                "abc@gmail.com.1a",
+                "abc@gmail.com.aa.au"
+        };
+        System.out.println("Testing Valid Emails ");
+        for(String vemail:validEmails){
+            System.out.println(vemail +"  "+vemail.matches(regex));
+        }
+        System.out.println("Testing Invalid Emails ");
+        for(String inemail:invalidEmails){
+            System.out.println(inemail +"  "+inemail.matches(regex));
         }
     }
 }
